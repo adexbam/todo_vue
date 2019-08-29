9dd6a1ffaee5ca45197b7a05db37ba1cb55988cf
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-08-29 16:15:24 +0200

    added necessary files

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
+    <ul class="todos">
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.label }}
+      </li>
+    </ul>
+    <button @click="removeTodo(todo)">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      currentTodo: ''
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+      this.currentTodo = '';
+    }
+  }
+};
+</script>
+
+<style>
+</style>
