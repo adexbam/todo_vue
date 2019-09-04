e4292bf9c13ef86b28fbc89745b60bbea4fff562
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-09-04 15:23:28 +0200

    Design Considerations, added Vue Material

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
+  <div class='container'>
+    <h1>My Todo List</h1>
+    <input v-model="currentTodo" @keydown.enter="addTodo(todo)" placeholder="Add a todo">
     <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id">
-        {{ todo.label }} <input type='checkbox'>
+      <li v-for='(todo, index) in todos' :key='todo.id'>
+        <input type='checkbox' v-model='todo.completed' >
+          <div> 
+          <span 
+            class="todo-item-label"
+            :class='{completed: todo.completed}' 
+            @dblclick='editTodo(todo)' 
+            v-if="!todo.edit">
+              {{todo.label}}
+          </span> 
+
+            <input v-else class="todo-item-edit" type="text" v-model='todo.label' @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.escape="doneEdit(todo)"> 
+          </div>
+        <button class='closeButton' @click="removeTodo(index)">Delete</button>
       </li>
     </ul>
-    <button @click="removeTodo(todo)">Delete</button>
   </div>
 </template>
 
@@ -15,17 +27,24 @@ export default {
   data() {
     return {
       todos: [],
-      currentTodo: ''
+      currentTodo: '',
+      editedTodo: null
     };
   },
   methods: {
     addTodo() {
-      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false, edit: false});
       this.currentTodo = '';
     },
-    removeTodo(todo) {
-      var index = this.todos.indexOf(todo);
+    removeTodo(index) {
+      //var index = this.todos.indexOf(todo);
       this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+     todo.edit = true
+    },
+    doneEdit(todo) {
+     todo.edit = false
     }
   }
 };
